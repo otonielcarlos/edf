946f7b858d556f189b52b8d5c5a62ad46b2ab910
Otoniel Carlos <otonielcarlosf@gmail.com>
2017-07-27 19:19:47 -0500

    contacto prueba

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,7 +12,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -24,7 +24,7 @@ $(function() {
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+
                     email: email,
                     message: message
                 },
@@ -35,7 +35,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje ha sido enviado </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
